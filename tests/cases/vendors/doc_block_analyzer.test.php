53a4c1802aa516794b16bde9e8ec3d3d7b85c3d6
Mark Story <mark@mark-story.com>
2009-03-10 08:40:56 -0400

    Removed some docBlockRules Building out Analyzer

--- tests/cases/vendors/doc_block_analyzer.test.php
+++ tests/cases/vendors/doc_block_analyzer.test.php
@@ -1,8 +1,81 @@
 <?php
-
 App::import('Vendor', 'ApiGenerator.DocBlockAnalyzer');
+App::import('Vendor', 'ApiGenerator.ClassDocumentor');
+
+/**
+ * A class to run rules against
+ *
+ **/
+class TestSubjectOne {
+	public $noDoc = '';
+/**
+ * This one has a complete doc string
+ *
+ * @var string
+ **/
+	public $hasDoc;
+
+	public function iHazNoDocz() {
+
+	}
+/**
+ * This func haz docz
+ *
+ * @return void
+ **/
+	public function iCanHazDocz() {
+
+	}
+/**
+ * I haz docs, but no param tags
+ *
+ * @return void
+ **/
+	public function iHazNoParams($one, $two, $three) {
+		
+	}
+/**
+ * This function has everything
+ *
+ * @param string $one Its the first param
+ * @param string $two the second param
+ * @param string $three the third param
+ * @link http://mark-story.com
+ * @return void
+ **/
+	public function bestestFunc($one, $two, $three) {
+
+	}
+}
+
+Mock::generate('DocBlockRule', 'MockDocBlockRule');
 
 class DocBlockAnalyzerTestCase extends CakeTestCase {
-
+/**
+ * test construction and rule building.
+ *
+ * @return void
+ **/
+	function testConstruction() {
+		$analyze = new DocBlockAnalyzer(array('MissingLink', 'IncompleteTags'));
+		$rules = $analyze->getRules();
+		$this->assertEqual(array_keys($rules), array('MissingLinkDocBlockRule', 'IncompleteTagsDocBlockRule'));
+	}
+/**
+ * test that the source setting only allows Documentors.
+ *
+ * @return void
+ **/
+	function testSourceSetting() {
+		$analyze = new DocBlockAnalyzer();
+		$reflection = new ClassDocumentor('TestSubjectOne');
+		$result = $analyze->setSource($reflection);
+		$this->assertTrue($result);
+		
+		$this->expectError();
+		$fail = new StdClass();
+		$result = $analyze->setSource($fail);
+		$this->assertFalse($result);
+	}
 }
 ?>
--- vendors/doc_block_analyzer.php
+++ vendors/doc_block_analyzer.php
@@ -35,11 +35,19 @@ class DocBlockAnalyzer {
  **/
 	protected $_ruleNames = array();
 /**
- * undocumented class variable
+ * Default rules
  *
  * @var string
  **/
-	protected $_defaultRules = array();
+	protected $_defaultRules = array(
+		'MissingLink', 'Empty', 'MissingParams', 'IncompleteTags'
+	);
+/**
+ * Current reflection objects being inspected.
+ *
+ * @var object
+ **/
+	protected $_reflection;
 /**
  * Constructor
  *
@@ -60,10 +68,66 @@ class DocBlockAnalyzer {
  **/
 	protected function _buildRules() {
 		foreach ($this->_ruleNames as $rule) {
-			
+			$className = $rule . 'DocBlockRule';
+			if (!class_exists($className, false)) {
+				trigger_error('Missing Rule Class ' . $className, E_USER_WARNING);
+				continue;
+			}
+			$rule = new $className();
+			if ($rule instanceof DocBlockRule) {
+				$this->_rules[] = $rule;
+			}
 		}
 	}
+/**
+ * Get the Descriptions and Names of the Rules being used.
+ *
+ * @return array
+ **/
+	public function getRules() {
+		$out = array();
+		foreach ($this->_rules as $rule) {
+			$name = get_class($rule);
+			$out[$name] = $rule->description;
+		}
+		return $out;
+	}
+/**
+ * Set the source for the Analyzation.  Expects either a ClassDocumentor or FunctionDocumentor instance.
+ *
+ * @param object $reflector Reflection Object to be inspected.
+ * @return boolean Success of setting source.
+ **/
+	public function setSource($reflector) {
+		if (!($reflector instanceof ClassDocumentor) && !($reflector instanceof FunctionDocumentor)) {
+			trigger_error(sprintf(
+				'Expects an instance of ClassDocumentor or FunctionDocumentor, %s was given', 
+				get_class($reflector)
+			), E_USER_WARNING);
+			return false;
+		}
+		$reflector->getAll();
+		$this->_reflection = $reflector;
+		return true;
+	}
+/**
+ * Analyze a Reflection object.
+ * 
+ * @param object $reflector Reflection Object to be inspected (optional)
+ * @return array Array of scoring information
+ **/
+	public function analyze($reflector = null) {
+		if ($reflector !== null) {
+			$valid = $this->setSource($reflector);
+			if (!$valid) {
+				return array();
+			}
+		}
+		
+	}
 }
+
+
 /**
  * Abstract Base Class for DocBlock Rules
  *
@@ -71,12 +135,19 @@ class DocBlockAnalyzer {
  **/
 abstract class DocBlockRule {
 /**
- * undocumented function
+ * Description of the rule
  *
+ * @var string
+ **/
+	public $description = '';
+/**
+ * setSubject - Set the array of doc block info to be looked at.
+ * 
+ * @param array $docArray Array of parsed docblock info to evaluate.
  * @return void
  **/
-	public function setSubject($reflection) {
-		$this->_subject = $reflection;
+	public function setSubject($docArray) {
+		$this->_subject = $docArray;
 	}
 /**
  * Score - Run the scoring system for this rule
@@ -85,4 +156,58 @@ abstract class DocBlockRule {
  * @return float Returns the float value (between 0 - 1) of the rule.
  **/
 	abstract public function score();
+}
+
+
+/**
+ * Check for missing @link tags
+ *
+ * @package default
+ **/
+class MissingLinkDocBlockRule extends DocBlockRule {
+	public $description = 'Missing @link tags';
+/**
+ * Check for a @link tag in the tags array.
+ *
+ * @return float
+ **/
+	public function score() {
+
+	}
+}
+
+class EmptyDocBlockRule extends DocBlockRule {
+	public $description = 'Check for empty doc string';
+/**
+ * score method
+ *
+ * @return float
+ **/
+	public function score() {
+		
+	}
+}
+
+class MissingParamsDocBlockRule extends DocBlockRule {
+	public $description = 'Check for any empty @param tags';
+/**
+ * score method
+ *
+ * @return float
+ **/
+	public function score() {
+		
+	}
+}
+
+class IncompleteTagsDocBlockRule extends DocBlockRule {
+	public $description = 'Check for Incomplete Tag strigs.';
+/**
+ * score method
+ *
+ * @return float
+ **/
+	public function score() {
+		
+	}
 }
